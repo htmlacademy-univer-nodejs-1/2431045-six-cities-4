9c71cbca90abcc98ffeb408c4025c832387b8cc6
q4w3e2r1 <artem.zarafutdinov@urfu.me>
2025-03-23 13:27:00 +0500

    done

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,4 @@ export * from './comment.type.js';
 export * from './offer.type.js';
 export * from './user.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
